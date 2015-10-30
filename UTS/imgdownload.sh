c2f979c8a54025d8956971829383f78e341d514f
Nurizka Fitrianingrum <nurizkaf96@gmail.com>
2015-10-30 07:07:22 +0700

    Create imgdownload.sh

---
+++ UTS/imgdownload.sh
@@ -0,0 +1,7 @@
+wget -nd -r -P /home/nurizkaf96/Documents/ii3160/ -A jpeg,jpg,png http://www.km.itb.ac.id/
+# nurizkaf96 change to your username
+# /Documents/ii3160 just change it anywhere
+
+rsync "/home/nurizkaf96/Documents/ii3160/" "home/nurizkaf96/Documents/Progif/"
+# /home/nurizkaf96/Documents/ii3160/ --> Main directory
+# home/nurizkaf96/Documents/Progif/ --> Backup directory
