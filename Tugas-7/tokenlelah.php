8c2f83ae302de8b1f0c2e727c9c5b1a6649fbea5
Nurizka Fitrianingrum <nurizkaf96@gmail.com>
2015-11-10 08:52:26 +0700

    Create tokenlelah.php

---
+++ Tugas-7/tokenlelah.php
@@ -0,0 +1,28 @@
+<?php
+
+$token = $_GET["id_token"];
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "nunukupluk";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO cinta (Nama, Jurusan)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+?>
