7be64ad74c3472ec06dfddfcbca66b0a6589a95d
Kupluk <mirfanp@s.itb.ac.id>
2015-11-12 00:40:20 +0700

    Update and rename tokenlelah.php to token.php

--- Tugas-7/token.php
+++ Tugas-7/token.php
@@ -1,28 +1,32 @@
 <?php
-
-$token = $_GET["id_token"];
-
+$token = $_POST["idtoken"];
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "nunukupluk";
-
+$dbname = "progif";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
-$sql = "INSERT INTO cinta (Nama, Jurusan)
-VALUES ('John', 'Doe', 'john@example.com')";
-
+$sql = "INSERT INTO tokens (id)
+VALUES ('$token')";
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
+$display = "SELECT id FROM tokens";
+$result = $conn->query($display);
 
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+echo "jos";
 $conn->close();
-
 ?>
